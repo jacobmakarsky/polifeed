234e9508a76392f01370b9cb842584c5e6b0f70b
Jacob Makarsky <jmm7293@psu.edu>
2020-01-12 02:17:43 -0500

    adding html and js code

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,41 @@
+/* The function */
 
-  fetch('resources/json/politicianData.json')
-  	.then(function (response) 
-  	{
-    	return response.json();
-  	})
-  	.then(function (data) 
-  	{
-    	appendData(data);
-  	})
-  	.catch(function (err) 
-  	{
-    	console.log(err);
+function json2table(json, classes) 
+{
+  var cols = Object.keys(json[0]);
+  
+  var headerRow = '';
+  var bodyRows = '';
+  
+  classes = classes || '';
+
+  function capitalizeFirstLetter(string) 
+  {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  cols.map(function(col) 
+  {
+    headerRow += '<th>' + capitalizeFirstLetter(col) + '</th>';
   });
 
- var mainContainer = document.getElementById("arData");
+  json.map(function(row) 
+  {
+    bodyRows += '<tr>';
+
+    cols.map(function(colName) 
+    {
+      bodyRows += '<td>' + row[colName] + '</td>';
+    })
+
+    bodyRows += '</tr>';
+  });
+
+  return '<table class="' +
+         classes +
+         '"><thead><tr>' +
+         headerRow +
+         '</tr></thead><tbody>' +
+         bodyRows +
+         '</tbody></table>';
+}
